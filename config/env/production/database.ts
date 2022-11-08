d280fd9b8562c93a4de99ac8dd5905d81ba4afcc
Peter Christiansen <peterjctech@gmail.com>
2022-11-08 08:01:04 -0700

    connect to google cloud

---
+++ config/env/production/database.ts
@@ -0,0 +1,11 @@
+export default ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: `/cloudsql/${env("INSTANCE_CONNECTION_NAME")}`,
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USER"),
+      password: env("DATABASE_PASSWORD"),
+    },
+  },
+});
