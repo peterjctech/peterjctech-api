0c18b91cda4f1adb743ef34ad8af0c17ddbde436
Peter Christiansen <peterjctech@gmail.com>
2022-11-11 02:27:23 -0700

    fix ability to change db with localhost

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,27 +1,12 @@
 export default [
-  "strapi::errors",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        directives: {
-          "script-src": ["'self'", "'unsafe-inline'", "cdn.jsdelivr.net"],
-          "img-src": [
-            "'self'",
-            "data:",
-            "cdn.jsdelivr.net",
-            "strapi.io",
-            `https://storage.googleapis.com/peterjctech-api.appspot.com`,
-          ],
-        },
-      },
-    },
-  },
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::logger",
-  "strapi::query",
-  "strapi::body",
-  "strapi::favicon",
-  "strapi::public",
+    "strapi::errors",
+    "strapi::security",
+    "strapi::cors",
+    "strapi::poweredBy",
+    "strapi::logger",
+    "strapi::query",
+    "strapi::body",
+    "strapi::session",
+    "strapi::favicon",
+    "strapi::public",
 ];
